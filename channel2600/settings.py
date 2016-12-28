b44aa7a865fbf283638c9f2966b679cb03be78e3
Jake Goritski <jgoritski@gmail.com>
2016-12-28 15:58:04 -0500

    staticfiles settings

--- channel2600/settings.py
+++ channel2600/settings.py
@@ -104,6 +104,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+)
 
 RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
 RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
