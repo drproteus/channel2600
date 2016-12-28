f74d69e0a368887f4438fffea6d9162de8056770
Jake Goritski <jgoritski@gmail.com>
2016-12-28 17:46:18 -0500

    remove debug console.logs

--- chan/static/chan/js/main.js
+++ chan/static/chan/js/main.js
@@ -20,13 +20,11 @@ function initThumbnails() {
   document.querySelectorAll('.thumb').forEach(function(item) {
     item.addEventListener('click', toggleThumbnail);
   });
-  console.log('foo');
 }
 
 function initReplyLinks() {
   document.querySelectorAll('.reply-to').forEach(function(item) {
     item.addEventListener('click', function(event) {
-      console.log('kasdjlas');
       var replyId = event.target.dataset['postId'];
       document.querySelector('#id_body').value += ('\n>>' + replyId + '\n');
     });
