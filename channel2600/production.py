594339bc690f303fdacc9129d47a685ca02a429c
Jake Goritski <jgoritski@gmail.com>
2016-12-28 16:57:57 -0500

    add allowed hosts

--- channel2600/production.py
+++ channel2600/production.py
@@ -2,5 +2,6 @@ from channel2600.settings import *
 import dj_database_url
 
 DEBUG=False
+ALLOWED_HOSTS = ["https://channel2600.herokuapp.com", "channel2600.herokuapp.com"]
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
