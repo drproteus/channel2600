0c6f9430a1bc00a700269f8c163f7adf9fc912a3
Jake Goritski <jgoritski@gmail.com>
2016-12-28 15:49:50 -0500

    production settings and Procfile for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn channel2600.wsgi --log-file -
---
+++ channel2600/production.py
@@ -0,0 +1,7 @@
+from channel2600 import settings
+import dj_database_url
+
+DEBUG=False
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
