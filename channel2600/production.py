ba4f2d1a64fba53148a83669d6e4fd33c503dde8
Jake Goritski <jgoritski@gmail.com>
2017-01-06 09:19:10 -0500

    get production secret key from environment variables

--- channel2600/production.py
+++ channel2600/production.py
@@ -1,7 +1,9 @@
 from channel2600.settings import *
 import dj_database_url
+import os
 
-DEBUG=False
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = False
 ALLOWED_HOSTS = ["https://channel2600.herokuapp.com", "channel2600.herokuapp.com"]
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
